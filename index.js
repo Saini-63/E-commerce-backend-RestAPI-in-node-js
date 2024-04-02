15f35ea2e57e42b3d168ffa53641bf498f24206c
Shubham Saini (Saini-63) <ShubhamSaini123DucatReactFinalProject@gmail.com>
2024-04-02 17:04:52 +0530

    Here we create server and install nodemon

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 import express from 'express';
 
+//PORT
+const PORT = 8000;
+
 //rest object
 const app = express();
+
+//route
+app.get('/', (req, res) => {
+    return res.status(200).send("<h1>Welcome to Node server</h1>");
+})
+
+//Listen
+app.listen(PORT, () => {
+    console.log(`Server  is running on port ${PORT}`);
+})
 
